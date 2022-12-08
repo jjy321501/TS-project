b29ee68f9efe1277d7eb988fc3d395044e2c706c
jjy321501 <jjy321501@naver.com>
2022-12-08 13:45:58 +0900

    타입스크립트 쓰는 이유

--- TS-study/first-project/js-only.js
+++ TS-study/first-project/js-only.js
@@ -1,11 +1,15 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1");
+// const input2 = document.getElementById("num2");
 
-function add(num1, num2) {
-  return num1 + num2;
+// function add(num1, num2) {
+// if(typeof) 분기문으로 오류제거 및 변수 앞 +로 숫자타입 변환
+if (typeof num1 === "number" && typeof num2 === "number") {
+  return num + num2;
 }
+return +num1 + +num2;
+// }
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   console.log(add(input1.value, input2.value));
 });
