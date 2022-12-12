5805dced663edb285577e6b6b683506b1ea5e8b8
jjy321501 <jjy321501@naver.com>
2022-12-12 11:01:54 +0900

    const-var-let

--- TS-study/intro-TS/app.js
+++ TS-study/intro-TS/app.js
@@ -1,5 +1,4 @@
-"use strict";
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-  console.log("Click!");
-});
+var myName = "Jun";
+// myName = 'Young'; // ! const 변경 불가능한 상수 재할당 불가.
+var age = 29;
+age = 27;
--- TS-study/intro-TS/app.ts
+++ TS-study/intro-TS/app.ts
@@ -1,5 +1,15 @@
-const button = document.querySelector("button")!;
+const myName = "Jun";
+// myName = 'Young'; // ! const 변경 불가능한 상수 재할당 불가.
 
-button.addEventListener("click", () => {
-  console.log("Click!");
-});
+let age = 29;
+
+age = 27;
+
+function plus(a: number, b: number) {
+  // var result;
+  let result;
+  result = a + b;
+  return result;
+}
+
+console.log(result);
