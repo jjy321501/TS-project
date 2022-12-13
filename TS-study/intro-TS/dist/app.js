fb5f2dffabc7bf624090987ea6faf8d7174354c4
jjy321501 <jjy321501@naver.com>
2022-12-13 15:24:41 +0900

    public private 접근 수정자

--- TS-study/intro-TS/dist/app.js
+++ TS-study/intro-TS/dist/app.js
@@ -1,13 +1,26 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log("Department: " + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department("Accounting");
+accounting.addEmployee("jun");
+accounting.addEmployee("young");
+// accounting.employees[2] = "anna"; // ! private 속성이므로 클래스 외부 접근 X
 accounting.describe();
-const accountingCopy = { name: "s", describe: accounting.describe };
-accountingCopy.describe();
+accounting.name = "NEW NAME";
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: "s", describe: accounting.describe };
+// accountingCopy.describe();
--- TS-study/intro-TS/src/app.ts
+++ TS-study/intro-TS/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,12 +9,28 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("jun");
+accounting.addEmployee("young");
+
+// accounting.employees[2] = "anna"; // ! private 속성이므로 클래스 외부 접근 X
+
 accounting.describe();
+accounting.name = "NEW NAME";
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: "s", describe: accounting.describe };
+// const accountingCopy = { name: "s", describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
