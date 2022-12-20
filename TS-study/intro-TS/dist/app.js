bb52f6ee94b73fa9f3b0e25e6800b8ad2a4fcf7b
jjy321501 <jjy321501@naver.com>
2022-12-20 14:06:04 +0900

    고급타입(Type guard)

--- TS-study/intro-TS/dist/app.js
+++ TS-study/intro-TS/dist/app.js
@@ -8,3 +8,45 @@ const e1 = {
     privileges: ["create-server"],
     startDate: new Date(),
 };
+function addCombine(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name); // 두 속성 name 속성이 있어서 정상 작동
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("startDate: " + emp.startDate);
+    }
+}
+// printEmployeeInformation(e1); // 필드가 설정된 값
+printEmployeeInformation({ name: "young", startDate: new Date() }); // prilvileges 즉석설정
+// class 를 활용한 타입가드
+class Car {
+    drive() {
+        console.log("Driving ...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving a truck ...");
+    }
+    loadCargo(amount) {
+        console.log("Loading cargo ..." + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    //   if ("loadCargo" in vehicle) {
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(10000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- TS-study/intro-TS/src/app.ts
+++ TS-study/intro-TS/src/app.ts
@@ -33,4 +33,60 @@ const e1: ElevatedEmployee = {
 type Combine = string | number;
 type Numberic = number | boolean;
 
-type Universal = Combine | Numberic;
+type Universal = Combine & Numberic; // number type
+
+function addCombine(a: Combine, b: Combine) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+// console.log(addCombine("a", "b")); // ab
+// console.log(addCombine(1, 3)); //4
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name); // 두 속성 name 속성이 있어서 정상 작동
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("startDate: " + emp.startDate);
+  }
+}
+
+// printEmployeeInformation(e1); // 필드가 설정된 값
+printEmployeeInformation({ name: "young", startDate: new Date() }); // prilvileges 즉석설정
+
+// class 를 활용한 타입가드
+class Car {
+  drive() {
+    console.log("Driving ...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck ...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo ..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  //   if ("loadCargo" in vehicle) {
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(10000);
+  }
+}
+useVehicle(v1);
+useVehicle(v2);
