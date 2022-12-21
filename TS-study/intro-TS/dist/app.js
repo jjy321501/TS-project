fd93f06d080afded128292ea55848475db90332e
jjy321501 <jjy321501@naver.com>
2022-12-21 16:35:35 +0900

    제네릭

--- TS-study/intro-TS/dist/app.js
+++ TS-study/intro-TS/dist/app.js
@@ -1,99 +1,19 @@
 "use strict";
-// interface Admin {
-//   name: string;
-//   privileges: string[];
-// }
-var _a;
-const e1 = {
-    name: "jun",
-    privileges: ["create-server"],
-    startDate: new Date(),
-};
-function addCombine(a, b) {
-    if (typeof a === "string" || typeof b === "string") {
-        // typeof 타입가드
-        return a.toString() + b.toString();
-    }
-    return a + b;
+// 내장 제네릭
+// const names: Array<string> = ["jun", "young"];
+// // names[0].split("");
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+// promise.then((data) => {
+// //   data.split(" ");
+// });
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
 }
-const result = addCombine("Max", "jun");
-result.split(" ");
-//optional chaining
-const fetchedUserData = {
-    id: "u1",
-    name: "jun",
-    job: {
-        title: "Free",
-        description: "Freelancer",
-    },
-};
-console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
-// const emptyInput = null;
-const emptyInput = ""; // '' 그냥 빈 문자열을 넣고싶을때 null,undefined 와 다른 처리가 필요하다.
-const storedData = emptyInput || "DEFAULT"; // => DEFAULT
-// const storedData = emptyInput ?? "DEFAULT"; // => ''
-console.log(storedData);
-function printEmployeeInformation(emp) {
-    console.log("Name: " + emp.name); // 두 속성 name 속성이 있어서 정상 작동
-    if ("privileges" in emp) {
-        // 리터럴 in 타입가드
-        console.log("Privileges: " + emp.privileges);
-    }
-    if ("startDate" in emp) {
-        console.log("startDate: " + emp.startDate);
-    }
-}
-// printEmployeeInformation(e1); // 필드가 설정된 값
-printEmployeeInformation({ name: "young", startDate: new Date() }); // prilvileges 즉석설정
-// class 를 활용한 타입가드
-class Car {
-    drive() {
-        console.log("Driving ...");
-    }
-}
-class Truck {
-    drive() {
-        console.log("Driving a truck ...");
-    }
-    loadCargo(amount) {
-        console.log("Loading cargo ..." + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    //   if ("loadCargo" in vehicle) {
-    if (vehicle instanceof Truck) {
-        // instanceof 타입가드
-        vehicle.loadCargo(10000);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    //   //if (animal instanceof Bird) { // !! => X 인터페이스는 자바스크립트로 컴파일 되지 않았으므로
-    //   if ("flyingSpeed" in animal) {
-    //     console.log("Moving with speed: " + animal.flyingSpeed);
-    //   }
-    let speed;
-    switch (animal.type) {
-        case "bird":
-            speed = animal.flyingSpeed;
-            break;
-        case "horse":
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log("Moving at speed: " + speed);
-}
-moveAnimal({ type: "bird", flyingSpeed: 1000 });
-// const userInputElement = <HTMLInputElement>document.getElementById("user-input")!; // HTML 타입 캐스팅 리액트 JSX와 혼동요지가 있음
-const userInputElement = document.getElementById("user-input"); // as 뒤에 타입캐스팅
-if (userInputElement) {
-    userInputElement.value = "Hi there!";
-}
-const errorBag = {
-    email: "Not a valid email!",
-    username: "Must start with a capital character!",
-};
+// console.log(merge({ name: "jun" }, { age: 28 })); // 정상 작동 케이스
+const mergedObj = merge({ name: "jun", hobbies: ["sports"] }, { age: 28 });
+// const mergedObj2 = merge({ name: "jun" }, { age: 28 }); // object 의 속성을 맞추지않아도 에러X
+console.log(mergedObj.age); // 사용할 수 있는 정보를 모르기때문에 타입캐스팅을 해야하는 번거로움이있다.
---
+++ TS-study/intro-TS/src/advanced.ts
@@ -0,0 +1,174 @@
+// interface Admin {
+//   name: string;
+//   privileges: string[];
+// }
+
+// interface Employee {
+//   name: string;
+//   startDate: Date;
+// }
+
+// interface ElevatedEmployee extends Admin, Employee {}
+
+// 위의 인터페이스로 구현한 방식과 동일하게 작동하지만
+// 인터섹션 타입
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "jun",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combine = string | number;
+type Numberic = number | boolean;
+
+type Universal = Combine & Numberic; // number type
+
+function addCombine(a: number, b: number): number;
+function addCombine(a: string, b: string): string;
+function addCombine(a: string, b: number): string;
+function addCombine(a: number, b: string): string;
+function addCombine(a: Combine, b: Combine) {
+  if (typeof a === "string" || typeof b === "string") {
+    // typeof 타입가드
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = addCombine("Max", "jun");
+result.split(" ");
+
+//optional chaining
+const fetchedUserData = {
+  id: "u1",
+  name: "jun",
+  job: {
+    title: "Free",
+    description: "Freelancer",
+  },
+};
+
+console.log(fetchedUserData?.job?.title);
+
+// const emptyInput = null;
+const emptyInput = ""; // '' 그냥 빈 문자열을 넣고싶을때 null,undefined 와 다른 처리가 필요하다.
+
+const storedData = emptyInput || "DEFAULT"; // => DEFAULT
+// const storedData = emptyInput ?? "DEFAULT"; // => ''
+
+console.log(storedData);
+
+// console.log(addCombine("a", "b")); // ab
+// console.log(addCombine(1, 3)); //4
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name); // 두 속성 name 속성이 있어서 정상 작동
+  if ("privileges" in emp) {
+    // 리터럴 in 타입가드
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("startDate: " + emp.startDate);
+  }
+}
+
+// printEmployeeInformation(e1); // 필드가 설정된 값
+printEmployeeInformation({ name: "young", startDate: new Date() }); // prilvileges 즉석설정
+
+// class 를 활용한 타입가드
+class Car {
+  drive() {
+    console.log("Driving ...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck ...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo ..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  //   if ("loadCargo" in vehicle) {
+  if (vehicle instanceof Truck) {
+    // instanceof 타입가드
+    vehicle.loadCargo(10000);
+  }
+}
+useVehicle(v1);
+useVehicle(v2);
+
+//구별된 유니언타입
+interface Bird {
+  type: "bird"; // type 이 Bird 여야하는 문자열 리터럴타입
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  //   //if (animal instanceof Bird) { // !! => X 인터페이스는 자바스크립트로 컴파일 되지 않았으므로
+  //   if ("flyingSpeed" in animal) {
+  //     console.log("Moving with speed: " + animal.flyingSpeed);
+  //   }
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log("Moving at speed: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 1000 });
+
+// const userInputElement = <HTMLInputElement>document.getElementById("user-input")!; // HTML 타입 캐스팅 리액트 JSX와 혼동요지가 있음
+const userInputElement = document.getElementById(
+  "user-input"
+) as HTMLInputElement; // as 뒤에 타입캐스팅
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there!";
+}
+
+// { email: 'Not a valid email' , username: 'Must start with a character!'}
+interface ErrorContainer {
+  [prop: string]: string;
+}
+const errorBag: ErrorContainer = {
+  email: "Not a valid email!",
+  username: "Must start with a capital character!",
+};
