687cfdad8a0ce5810af6f95f3089b61fe52f7bdc
jjy321501 <jjy321501@naver.com>
2022-12-21 13:22:03 +0900

    고급 타입(Function Overload), 인덱스 속성,선택적 체이닝

--- TS-study/intro-TS/dist/app.js
+++ TS-study/intro-TS/dist/app.js
@@ -3,6 +3,7 @@
 //   name: string;
 //   privileges: string[];
 // }
+var _a;
 const e1 = {
     name: "jun",
     privileges: ["create-server"],
@@ -10,13 +11,31 @@ const e1 = {
 };
 function addCombine(a, b) {
     if (typeof a === "string" || typeof b === "string") {
+        // typeof 타입가드
         return a.toString() + b.toString();
     }
     return a + b;
 }
+const result = addCombine("Max", "jun");
+result.split(" ");
+//optional chaining
+const fetchedUserData = {
+    id: "u1",
+    name: "jun",
+    job: {
+        title: "Free",
+        description: "Freelancer",
+    },
+};
+console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
+const emptyInput = null;
+// const emptyInput = '';  // '' 그냥 빈 문자열을 넣고싶을때 null,undefined 와 다른 처리가 필요하다.
+const storedData = emptyInput || "DEFAULT"; //
+console.log(storedData);
 function printEmployeeInformation(emp) {
     console.log("Name: " + emp.name); // 두 속성 name 속성이 있어서 정상 작동
     if ("privileges" in emp) {
+        // 리터럴 in 타입가드
         console.log("Privileges: " + emp.privileges);
     }
     if ("startDate" in emp) {
@@ -45,8 +64,35 @@ function useVehicle(vehicle) {
     vehicle.drive();
     //   if ("loadCargo" in vehicle) {
     if (vehicle instanceof Truck) {
+        // instanceof 타입가드
         vehicle.loadCargo(10000);
     }
 }
 useVehicle(v1);
 useVehicle(v2);
+function moveAnimal(animal) {
+    //   //if (animal instanceof Bird) { // !! => X 인터페이스는 자바스크립트로 컴파일 되지 않았으므로
+    //   if ("flyingSpeed" in animal) {
+    //     console.log("Moving with speed: " + animal.flyingSpeed);
+    //   }
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log("Moving at speed: " + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 1000 });
+// const userInputElement = <HTMLInputElement>document.getElementById("user-input")!; // HTML 타입 캐스팅 리액트 JSX와 혼동요지가 있음
+const userInputElement = document.getElementById("user-input"); // as 뒤에 타입캐스팅
+if (userInputElement) {
+    userInputElement.value = "Hi there!";
+}
+const errorBag = {
+    email: "Not a valid email!",
+    username: "Must start with a capital character!",
+};
--- TS-study/intro-TS/src/app.ts
+++ TS-study/intro-TS/src/app.ts
@@ -35,12 +35,40 @@ type Numberic = number | boolean;
 
 type Universal = Combine & Numberic; // number type
 
+function addCombine(a: number, b: number): number;
+function addCombine(a: string, b: string): string;
+function addCombine(a: string, b: number): string;
+function addCombine(a: number, b: string): string;
 function addCombine(a: Combine, b: Combine) {
   if (typeof a === "string" || typeof b === "string") {
+    // typeof 타입가드
     return a.toString() + b.toString();
   }
   return a + b;
 }
+
+const result = addCombine("Max", "jun");
+result.split(" ");
+
+//optional chaining
+const fetchedUserData = {
+  id: "u1",
+  name: "jun",
+  job: {
+    title: "Free",
+    description: "Freelancer",
+  },
+};
+
+console.log(fetchedUserData?.job?.title);
+
+const emptyInput = null;
+// const emptyInput = '';  // '' 그냥 빈 문자열을 넣고싶을때 null,undefined 와 다른 처리가 필요하다.
+
+const storedData = emptyInput || "DEFAULT"; //
+
+console.log(storedData);
+
 // console.log(addCombine("a", "b")); // ab
 // console.log(addCombine(1, 3)); //4
 
@@ -49,6 +77,7 @@ type UnknownEmployee = Employee | Admin;
 function printEmployeeInformation(emp: UnknownEmployee) {
   console.log("Name: " + emp.name); // 두 속성 name 속성이 있어서 정상 작동
   if ("privileges" in emp) {
+    // 리터럴 in 타입가드
     console.log("Privileges: " + emp.privileges);
   }
   if ("startDate" in emp) {
@@ -85,8 +114,60 @@ function useVehicle(vehicle: Vehicle) {
   vehicle.drive();
   //   if ("loadCargo" in vehicle) {
   if (vehicle instanceof Truck) {
+    // instanceof 타입가드
     vehicle.loadCargo(10000);
   }
 }
 useVehicle(v1);
 useVehicle(v2);
+
+//구별된 유니언타입
+interface Bird {
+  type: "bird"; // type 이 Bird 여야하는 문자열 리터럴타입
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  //   //if (animal instanceof Bird) { // !! => X 인터페이스는 자바스크립트로 컴파일 되지 않았으므로
+  //   if ("flyingSpeed" in animal) {
+  //     console.log("Moving with speed: " + animal.flyingSpeed);
+  //   }
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log("Moving at speed: " + speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 1000 });
+
+// const userInputElement = <HTMLInputElement>document.getElementById("user-input")!; // HTML 타입 캐스팅 리액트 JSX와 혼동요지가 있음
+const userInputElement = document.getElementById(
+  "user-input"
+) as HTMLInputElement; // as 뒤에 타입캐스팅
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there!";
+}
+
+// { email: 'Not a valid email' , username: 'Must start with a character!'}
+interface ErrorContainer {
+  [prop: string]: string;
+}
+const errorBag: ErrorContainer = {
+  email: "Not a valid email!",
+  username: "Must start with a capital character!",
+};
