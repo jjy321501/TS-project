89462937cde7c914d67dc5a5311d7126406d46a9
jjy321501 <jjy321501@naver.com>
2022-12-22 11:51:28 +0900

    제너릭 제약조건,keyof

--- TS-study/intro-TS/dist/app.js
+++ TS-study/intro-TS/dist/app.js
@@ -15,5 +15,22 @@ function merge(objA, objB) {
 }
 // console.log(merge({ name: "jun" }, { age: 28 })); // 정상 작동 케이스
 const mergedObj = merge({ name: "jun", hobbies: ["sports"] }, { age: 28 });
-// const mergedObj2 = merge({ name: "jun" }, { age: 28 }); // object 의 속성을 맞추지않아도 에러X
-console.log(mergedObj.age); // 사용할 수 있는 정보를 모르기때문에 타입캐스팅을 해야하는 번거로움이있다.
+console.log(mergedObj.hobbies); // 사용할 수 있는 정보를 모르기때문에 타입캐스팅을 해야하는 번거로움이있다.
+function countAndDescribe(element) {
+    let descriptionText = "Got no value";
+    if (element.length === 1) {
+        descriptionText = "Got 1 element.";
+    }
+    else if (element.length > 1) {
+        descriptionText = "Got " + element.length + " elements.";
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(["Hi there", "Cooking"])); // Array(2), 2elements
+// console.log(countAndDescribe("123")); // '123' , 3elements
+// console.log(countAndDescribe(123)); // number 타입은 length를 사용할 수 없음
+// keyof 제약조건으로 상세히 타입을 줄수있다.
+function extractAndConvert(obj, key) {
+    return "Value: " + obj[key];
+}
+console.log(extractAndConvert({ name: "jun" }, "name"));
--- TS-study/intro-TS/src/app.ts
+++ TS-study/intro-TS/src/app.ts
@@ -12,11 +12,37 @@
 // //   data.split(" ");
 // });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends {}, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 // console.log(merge({ name: "jun" }, { age: 28 })); // 정상 작동 케이스
 
 const mergedObj = merge({ name: "jun", hobbies: ["sports"] }, { age: 28 });
-// const mergedObj2 = merge({ name: "jun" }, { age: 28 }); // object 의 속성을 맞추지않아도 에러X
-console.log(mergedObj.age); // 사용할 수 있는 정보를 모르기때문에 타입캐스팅을 해야하는 번거로움이있다.
+console.log(mergedObj.hobbies); // 사용할 수 있는 정보를 모르기때문에 타입캐스팅을 해야하는 번거로움이있다.
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["Hi there", "Cooking"])); // Array(2), 2elements
+// console.log(countAndDescribe("123")); // '123' , 3elements
+// console.log(countAndDescribe(123)); // number 타입은 length를 사용할 수 없음
+
+// keyof 제약조건으로 상세히 타입을 줄수있다.
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+console.log(extractAndConvert({ name: "jun" }, "name"));
