890d56241c389a076e3035a4d05f5eeb9bdda453
jjy321501 <jjy321501@naver.com>
2022-12-27 14:20:35 +0900

    데코레이터(class, method, accessor, property, parameter)

--- TS-study/intro-TS/dist/app.js
+++ TS-study/intro-TS/dist/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Logger(logStr) {
     console.log("LOGGER FACTORY");
     return function (constructor) {
@@ -14,26 +17,83 @@ function Logger(logStr) {
 }
 function WithTemplate(template, hookId) {
     console.log("TEMPLATE FACTORY");
-    return function (constructor) {
-        console.log("Rendering Template");
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector("h1").innerText = p.name;
-        }
+    return function (originalConstructor) {
+        return class extends originalConstructor {
+            constructor(..._) {
+                super();
+                console.log("Rendering Template");
+                const hookEl = document.getElementById(hookId);
+                // const p = new originalConstructor();
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector("h1").innerText = this.name; // 오리지널 생성자를 부르지않으니 p.name 대신 this. 이용
+                }
+            }
+        };
     };
 }
 // @Logger("LOGGING - PERSON")
-let Person = class Person {
+let Human = class Human {
     constructor() {
         this.name = "jun";
         console.log("Creating person object...");
     }
 };
-Person = __decorate([
+Human = __decorate([
     Logger("LOGGING"),
     WithTemplate("<h1>My Person Object </h1>", "app")
-], Person);
-const pers = new Person();
+], Human);
+const pers = new Human();
 console.log(pers);
+// ---
+function Log(target, propertyName) {
+    console.log("Property decorator!");
+    console.log(target, propertyName);
+}
+function Log2(target, name, descriptor) {
+    console.log("Accessor decorator!");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, desciptor) {
+    console.log("Method decorator");
+    console.log(target);
+    console.log(name);
+    console.log(desciptor);
+}
+function Log4(target, name, position) {
+    console.log("Parameter decorator");
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error("Invalid price");
+        }
+    }
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
+const p1 = new Product("Book", 19);
+const p2 = new Product("Book 2", 19);
--- TS-study/intro-TS/src/app.ts
+++ TS-study/intro-TS/src/app.ts
@@ -8,27 +8,88 @@ function Logger(logStr: string) {
 
 function WithTemplate(template: string, hookId: string) {
   console.log("TEMPLATE FACTORY");
-  return function (constructor: any) {
-    console.log("Rendering Template");
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.innerText = p.name;
-    }
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("Rendering Template");
+        const hookEl = document.getElementById(hookId);
+        // const p = new originalConstructor();
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.innerText = this.name; // 오리지널 생성자를 부르지않으니 p.name 대신 this. 이용
+        }
+      }
+    };
   };
 }
 
 // @Logger("LOGGING - PERSON")
 @Logger("LOGGING")
 @WithTemplate("<h1>My Person Object </h1>", "app")
-class Person {
+class Human {
   name = "jun";
   constructor() {
     console.log("Creating person object...");
   }
 }
 
-const pers = new Person();
+const pers = new Human();
 
 console.log(pers);
+
+// ---
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+function Log3(
+  target: any,
+  name: string | Symbol,
+  desciptor: PropertyDescriptor
+) {
+  console.log("Method decorator");
+  console.log(target);
+  console.log(name);
+  console.log(desciptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price");
+    }
+  }
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+const p1 = new Product("Book", 19);
+const p2 = new Product("Book 2", 19);
