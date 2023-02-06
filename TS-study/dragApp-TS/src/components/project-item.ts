525d469e6764b1dcfbf397f2b8afc7fffbcd4c07
jjy321501 <jjy321501@naver.com>
2023-02-06 14:55:37 +0900

    webpack (ts-loader 패키지 소스맵)

--- TS-study/dragApp-TS/src/components/project-item.ts
+++ TS-study/dragApp-TS/src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Draggable } from "../models/drag-drop.js";
-import { Project } from "../models/project.js";
-import Component from "./base.js";
-import { autobind } from "../decorators/autobind.js";
+import { Draggable } from "../models/drag-drop";
+import { Project } from "../models/project";
+import Component from "./base";
+import { autobind } from "../decorators/autobind";
 
 // ProjectItem Class
 export class ProjectItem
