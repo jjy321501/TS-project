c7b952d523a2fae4e63ae245078c272d524542c2
jjy321501 <jjy321501@naver.com>
2023-02-13 12:05:30 +0900

    mapApp (google map 에 마커와 함께 표시하기)

--- TS-study/mapApp-TS/src/app.ts
+++ TS-study/mapApp-TS/src/app.ts
@@ -5,6 +5,8 @@ const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 const GOOGLE_API_KEY = "AIzaSyApujS4kHDHc6hDB0mSvKEuUgIv2hwIxQ8";
 
+declare var google: any;
+
 type GoogleGeocodingResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
   status: "OK" | "ZERO_RESULTS";
@@ -26,6 +28,15 @@ function searchAddressHandler(event: Event) {
         throw new Error("Could not fetch location!");
       }
       const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 16,
+      });
+
+      new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
       console.log(response);
     })
     .catch((err) => {
