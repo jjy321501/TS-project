0ef8c7ea6e270e90273391b1d2b2da7fd3896f06
jjy321501 <jjy321501@naver.com>
2023-02-14 12:53:58 +0900

    react-ts (useRef  사용자 입력 받기)

--- TS-study/react-ts/src/App.tsx
+++ TS-study/react-ts/src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "완주 완료" }];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
   return (
     <div className="App">
       {/* A component that adds todos */}
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
